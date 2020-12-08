b529fc12d3447f0833c202dcb4618170832099c3
Mathew Teague <mathew.teague@yewtreeweb.co.uk>
2020-12-08 16:43:20 +0000

    created filter with static queries

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import PizzaList from '../components/PizzaList'
+import ToppingsFilter from '../components/ToppingsFilter'
 
 export default function PizzaPage ({ data }) {
   console.log(data)
   const pizzas = data.pizzas.nodes
   return (
     <>
+      <ToppingsFilter />
       <PizzaList pizzas={pizzas} />
     </>
   )
